78e95dc59b154cadce284f788b8afae609757c62
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2023-11-08 21:58:41 -0500

    feat: search bar update

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,37 +1,36 @@
-'use client'
+"use client";
 
-import { useSearchParams, useRouter } from 'next/navigation';
-import React, { FormEvent, useState } from 'react';
-
+import { useSearchParams, useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 function SearchBar() {
+  const router = useRouter();
+  const search = useSearchParams();
 
-  const router = useRouter()
-  const search = useSearchParams()
+  const [searchQuery, setSearchQuery] = useState<string | null>(
+    search ? search.get("q") : null
+  );
 
-  const [searchQuery, setSearchQuery] = useState<string | null>(search? search.get('q') : null)
+  const onSearch = (e: FormEvent) => {
+    e.preventDefault();
 
-  const onSearch =(e: FormEvent)=>{
+    const encodedSearchQuery = encodeURI(searchQuery || "");
 
-    e.preventDefault()
-
-    const encodedSearchQuery = encodeURI(searchQuery|| "")
-
-    setSearchQuery("")
-    router.push(`/search?q=${encodedSearchQuery}`)
-
-
-  }
+    setSearchQuery("");
+    router.push(`/search?q=${encodedSearchQuery}`);
+  };
   return (
-    <form onSubmit={onSearch}
-    className='dark:text-black-400 text-black text-2xl mx-auto 
-    max-w-md'>
-
-
-      <input value={searchQuery ||""}
-      onChange={e=>setSearchQuery(e.target.value)}
-
-      className='p-1 rounded-md'
+    <form
+      onSubmit={onSearch}
+      className="dark:text-black-400 text-black text-2xl mx-auto 
+    max-w-md"
+    >
+      <input
+        type="text"
+        value={searchQuery || ""}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="p-1 rounded-md"
+        placeholder="search for posts!"
       />
     </form>
   );
