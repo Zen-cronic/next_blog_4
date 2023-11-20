0682d767b11182380b6cc970187419b8c5ad8112
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2023-11-19 22:01:02 -0500

    minor: remove underline

--- src/app/components/ListItem.tsx
+++ src/app/components/ListItem.tsx
@@ -16,7 +16,7 @@ function ListItem({post}: Props) {
   const formattedDate = getFormattedDate(date)
   return (
     <li className="mt-4 text-2xl dark:text-white/90">
-        <Link className="underline hover:text-black/70 dark:hover:text-white" href={`/posts/${id}`}>{title}</Link>
+        <Link className=" hover:text-black/70 dark:hover:text-white" href={`/posts/${id}`}>{title}</Link>
         <br />
         <p className='text-base font-bold dark:text-slate-400 '>{subtitle}</p>
         <p className="text-xs mt-1">{formattedDate}</p>
